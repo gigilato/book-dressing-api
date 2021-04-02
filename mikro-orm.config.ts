d89d832fb42ce75e98c0147489f9f793ff3a80dc
gigilato <gislain.moreira@gmail.com>
2021-04-02 21:03:36 +0200

    new mikro orm conf

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -1,3 +1,5 @@
+import { Connection, IDatabaseDriver } from '@mikro-orm/core'
+import { MikroOrmModuleOptions } from '@mikro-orm/nestjs'
 import * as dotenv from 'dotenv'
 import dotenvExpand from 'dotenv-expand'
 import { Book } from './src/modules/book/book.entity'
@@ -7,13 +9,20 @@ import { User } from './src/modules/user/user.entity'
 const env = dotenv.config()
 dotenvExpand(env)
 
-export default {
+const config: MikroOrmModuleOptions<IDatabaseDriver<Connection>> = {
   entities: [User, Book, Loan],
   type: 'postgresql',
   clientUrl: process.env?.DATABASE_URL,
+  driverOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   migrations: {
     path: './migrations',
     tableName: 'migrations',
     transactional: true,
   },
 }
+
+export default config
